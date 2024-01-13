4017d219f3910d568bc392ac5db6e93d57d2fb2f
Bhupendra Chauhan <bhupendrachauhan6194@gmail.com>
2024-01-13 21:29:14 +0530

    Day 3 updated

---
+++ Day3/arrowFunctions.js
@@ -0,0 +1,45 @@
+// 1. function declaration
+// 2. function expression
+// 3. Arrow functions
+
+
+// Arrow Function 
+
+// const hello3 = (name) => {
+//       console.log(`hello ${name} from arrow function`);
+//     };
+
+// hello3("name");
+
+// const hello4 = (name, age) => {
+//     return `${name}, ${age}`
+// }
+
+// arrrow function implicit return
+
+// const hello5 = (name, age) => `${name},${age}`;
+
+// arrow function with one parameter
+
+// const is18 = (age) => {
+//   return age >= 18;
+// };
+
+// implicit return
+
+// const is18 = (age) => age >= 18;
+// console.log(is18(16))
+
+// if your function takes 1 arg only then you can omit ();
+
+// const is18 = age => age >= 18;
+
+
+
+// you cannot omit () with more than 1 args
+
+// const hello5 = (name, age) => `${name},${age}`;
+
+
+// you cannot omit () if function take no args
+// const hello = () => `hello world`
---
+++ Day3/destructuring.js
@@ -0,0 +1,90 @@
+
+// const person = {
+//     firstName : "john",
+//     lastName : "doe",
+//     age : 25,
+// }
+
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
+
+//  Object Destructuring;
+
+// const {firstName, lastName, age} = person;
+// console.log(firstName, age);
+
+// change variable name;
+
+// const { firstName: fname, age } = person;
+
+// console.log(fname, age);
+
+// Array destructuring
+
+// const fruits = ["Apple", "Mango", "grapes"];
+
+// const fruit1 = fruits[0];
+// const fruit2 = fruits[1];
+// const fruit3 = fruits[2];
+
+// console.log(fruit1, fruit2, fruit3);
+
+// const [fruit1, fruit2, fruit3] = fruits;
+
+// console.log(fruit1, fruit2, fruit3);
+
+// skip indexes
+
+// const [fruit1, , fruit3] = fruits;
+
+// console.log(fruit1, fruit3);
+
+
+// function myFunc(properties) {
+//   const { firstName, lastName, age } = properties;
+//   console.log(firstName, lastName, age);
+// }
+
+// function myFunc({ firstName, lastName, age }) {
+//   console.log(firstName, lastName, age);
+// }
+
+// myFunc({
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 25,
+// });
+
+// const bigObject = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 25,
+//     gender: "male",
+//     city: "Ambala",
+//     state: "Haryana",
+//     country: "India",
+//     pincode: "111111",
+//     phone: "7878787878",
+//   };
+  
+//   const { firstName, lastName, age } = bigObject;
+  
+//   const person = {
+//     fName: firstName,
+//     lName: lastName,
+//     age: age,
+//   };
+  
+//   shortcut
+//   const person = {
+//     firstName,
+//     lastName,
+//     age,
+//     myOwnProp: "myOwnProp",
+//     myOwnNestedObj: {
+//       prop1: "prop1",
+//     },
+//   };
+  
+//   console.log(person);
