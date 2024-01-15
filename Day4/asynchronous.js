6113131c91a243ce09ddbeac7263b4fa44a76c13
Bhupendra Chauhan <bhupendrachauhan6194@gmail.com>
2024-01-15 23:03:52 +0530

    added promises, async functions

---
+++ Day4/asynchronous.js
@@ -0,0 +1,66 @@
+// Js: synchronous ? asynchornous ?
+
+// console.log("something");
+// console.log("something2");
+
+// //very slow work(less priority)
+
+// for(let i = 0; i<10; i++)
+// {
+//     console.log("value of i" + [i]);
+ 
+// }
+
+
+
+
+//webAPIS
+// asynchronous functionality provided by webAPIS ?
+
+
+//  setTimeout
+
+// Needs 2 args
+// 1: function
+// 2: time in ms
+
+// function hello(){
+//     console.log("hello");
+// }
+// setTimeout(hello, 2000);
+
+
+
+
+// console.log("script start.....");
+
+// setTimeout(() => {
+//     console.log("setTimeout....");
+// }, 2000);
+
+// console.log("script end...");
+
+
+
+
+// console.log("script start.....");
+
+// const setTimeoutId = setTimeout(() => {
+//   console.log("SetTimeout1");
+// }, 1000);
+
+// clearTimeout(setTimeoutId);
+
+// console.log("script end");
+// // code that takes 1 hr.
+
+
+
+
+
+
+// setInterval
+
+// setInterval(() => {
+//   console.log("Hello");
+// }, 1000);
---
+++ Day4/promises.js
@@ -0,0 +1,19 @@
+
+
+import friedRicePromise from "./myPromises.js";
+
+console.log("Script Start");
+
+setTimeout(() => {
+  console.log("Hello from settimeout");
+}, 0);
+
+friedRicePromise()
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((errorValue) => {
+    console.log(errorValue);
+  });
+
+console.log("script end");
