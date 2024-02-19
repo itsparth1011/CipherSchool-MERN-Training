d436d30a571e30796e3e51ef5e4e9be607831a76
Bhupendra Chauhan <bhupendrachauhan6194@gmail.com>
2024-02-19 23:10:11 +0530

    added backend basics

--- 17-Day/Backend/index.js
+++ 17-Day/Backend/index.js
@@ -1,17 +1,50 @@
 const express = require("express");
-
+const todos = require("./db.json")
+const bodyParser = require("body-parser");
 const app = express();
+const port = 8000;
 
 /*
 {
     get: (req, res) => {}
     get, post, put, patch, delete
 }
+cycle of req and res:_
+
+//browser/ postman can send request
+
+//express can react request
+//express can send response
+
+// browser  and postman can read response
+
 */
 
-app.get("/", (req, res) => {});
+app.use(bodyParser.json())
 
-const port = 8000;
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from sever" })
+});
+
+// get all todos:
+app.get("/api/todos", (req, res) => {
+    res.json(todos)
+})
+
+// Get single todo:
+app.get("/api/todos/:id", (req, res) => {
+    const id = req.params.id;
+    const todo = todos.find((todo) => todo.id === id)
+    if (todo) {
+        res.status(200).json(todo);
+    } else {
+        res.status(400).json({ message: `No todo presnt with id ${id}` })
+    }
+})
+
+//create todo
+app.post("./api/todos")
+
 
 app.listen(port, () => {
     console.log(`App is running on port : ${port}`)
